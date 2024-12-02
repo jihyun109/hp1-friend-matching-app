1ef453b9f0d86466db0384fc0a75dbd1a318e393
jihyun109 <wlguswlgus989@gmail.com>
2024-12-02 15:50:47 +0900

    refactor: jwt 클레임에 userId 포함

--- src/main/java/com/hp1/friendmatchingapp/jwt/JwtUtil.java
+++ src/main/java/com/hp1/friendmatchingapp/jwt/JwtUtil.java
@@ -22,9 +22,10 @@ public class JwtUtil {
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1시간 유효
 
-    public String generateToken(String username) {
+    public String generateToken(String username, Long userId) {
         SecretKey secretKey = new SecretKeySpec(signingKey.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());;
         return Jwts.builder()
+                .setClaims(Map.of("userId", userId))
                 .setClaims(Map.of("userName", username))    // JWT에 사용자 이름 저장
                 .setIssuedAt(new Date())  // 발급 시간
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // 만료 시간

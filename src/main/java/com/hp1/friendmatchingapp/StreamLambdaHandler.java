11a8413f7f86696ca1507b973ac5e7aebec30759
jihyun109 <wlguswlgus989@gmail.com>
2024-12-11 20:19:59 +0900

    refactor: cors 설정 수정

--- src/main/java/com/hp1/friendmatchingapp/StreamLambdaHandler.java
+++ src/main/java/com/hp1/friendmatchingapp/StreamLambdaHandler.java
@@ -7,10 +7,12 @@ import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Base64;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
@@ -23,6 +25,8 @@ public class StreamLambdaHandler implements RequestStreamHandler {
                     .asyncInit()
                     .springBootApplication(FriendmatchingappApplication.class)
                     .buildAndInitialize();
+//            handler.getServletContext().setInitParameter("binaryContentTypes", "multipart/form-data,application/octet-stream,image/png,image/jpeg");
+            handler.getContainerConfig().addBinaryContentTypes("multipart/form-data");
 
         } catch(ContainerInitializationException e){
             throw new RuntimeException("Spring Boot Application 실행 실패", e);
@@ -32,5 +36,6 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
         handler.proxyStream(input, output, context);
+
     }
 }
